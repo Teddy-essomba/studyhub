298e7f3852ed15eddb08fd631275f9c5755b2c0b
Teddy essomba <teddyjules02@email.com>
2026-05-19 12:17:40 +0100

    testing a task api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import hello
+from .views import task_list
 
 urlpatterns = [
-    path('hello/', hello),
+    path('tasks/', task_list),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,22 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+"""
 @api_view(['GET'])
 def hello(request):
     return Response({
         "message": "Hello from Django!"
     })
+"""
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
