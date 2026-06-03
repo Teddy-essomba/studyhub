211ad75803cb46d742e0cf6a30835c57a8bd7b6f
Teddy essomba <teddyjules02@email.com>
2026-06-03 12:30:42 +0100

    fixing app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,78 @@
+import { useEffect, useState } from 'react';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 
-return (
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('');
+
+  async function fetchTasks() {
+    const response = await fetch('http://127.0.0.1:8000/api/tasks/');
+    const data = await response.json();
+    setTasks(data);
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+
+  async function addTask(e) {
+    e.preventDefault();
+
+    const response = await fetch('http://127.0.0.1:8000/api/tasks/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: title,
+        completed: false,
+      }),
+    });
+
+
+    const newTask = await response.json();
+    setTasks([...tasks, newTask]);
+    setTitle('');
+  }
+
+  async function toggleTask(task) {
+  const response = await fetch(`http://127.0.0.1:8000/api/tasks/${task.id}/`, {
+
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify({
+      completed: !task.completed,
+    }),
+
+  });
+
+  const updatedTask = await response.json();
+  setTasks(tasks.map((t) => {
+    if (t.id === updatedTask.id) {
+      return updatedTask;
+    }
+    return t;
+  }));
+
+}
+
+
+async function deleteTask(id) {
+  const response = await fetch(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+    method: 'DELETE',
+  });
+
+  if (response.ok) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+}
+
+  return (
   <div>
     <h1>StudyHub Tasks</h1>
 
@@ -18,3 +89,6 @@ return (
     />
   </div>
 );
+}
+
+export default App;
